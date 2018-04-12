4fc64060ad95dc5d4a4aaa3d604981423acbedbf
alexluong <aluong@crimson.ua.edu>
2018-04-12 15:05:32 -0500

    Added some extra styling for side panel

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import { Provider } from 'react-redux';
-import { Router } from 'react-router';
-import { Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history'
+import React, { Component }     from 'react';
+import { Provider }             from 'react-redux';
+import { Router }               from 'react-router';
+import { Route, Switch }        from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 // Layout
-import TopPanel from './components/top-panel/TopPanel';
+import TopPanel  from './components/top-panel/TopPanel';
 import SidePanel from './components/SidePanel';
 
 // Pages
 import DashboardListPage  from './components/dashboard/DashboardListPage';
-import DashboardPage  from './components/dashboard/DashboardPage';
-import ProgressPage   from './components/ProgressPage'    ;
-import Profile        from './components/Profile'         ;
-import Settings       from './components/Settings'        ; 
-import SignInPage     from './components/auth/SignInPage' ;
-import SignOutPage    from './components/auth/SignOutPage';
+import DashboardPage      from './components/dashboard/DashboardPage'    ;
+import ProgressPage       from './components/ProgressPage'               ;
+import Profile            from './components/Profile'                    ;
+import Settings           from './components/Settings'                   ; 
+import SignInPage         from './components/auth/SignInPage'            ;
+import SignOutPage        from './components/auth/SignOutPage'           ;
 
 export const history = createBrowserHistory();
 class App extends Component {
@@ -24,10 +24,10 @@ class App extends Component {
       <Provider store={this.props.store} >
         <Router history={history}>
           <Switch>
-            <Route path="/signin"  component={SignInPage} />
-            <Route path="/signup"  component={SignInPage} />
+            <Route path="/signin"  component={SignInPage}  />
+            <Route path="/signup"  component={SignInPage}  />
             <Route path="/signout" component={SignOutPage} />
-            <Route path="/" component={MainApp} />
+            <Route path="/"        component={MainApp}     />
           </Switch>
         </Router>
       </Provider>
@@ -44,10 +44,10 @@ class MainApp extends Component {
         <main className="main">
           <Switch>
             <Route path='/dashboard/list' component={DashboardListPage} />
-            <Route path='/dashboard' component={DashboardPage} />
-            <Route path='/progress'  component={ProgressPage} />
-            <Route path='/profile'   component={Profile} />
-            <Route path='/settings'  component={Settings} />
+            <Route path='/dashboard'      component={DashboardPage}     />
+            <Route path='/progress'       component={ProgressPage}      />
+            <Route path='/profile'        component={Profile}           />
+            <Route path='/settings'       component={Settings}          />
           </Switch>
         </main>
       </div>
--- client/src/components/SidePanel.js
+++ client/src/components/SidePanel.js
@@ -5,11 +5,11 @@ export default class SidePanel extends Component {
   render() {
     return (
       <ul className="side-panel">
-        <li className="side-panel__item">LOGO</li>
-        <li className="side-panel__item"><NavLink to="/dashboard">Dashboard</NavLink></li>
-        <li className="side-panel__item"><NavLink to="/progress">Progress</NavLink></li>
-        <li className="side-panel__item"><NavLink to="/profile">Profile</NavLink></li>
-        <li className="side-panel__item"><NavLink to="/settings">Settings</NavLink></li>
+        <li className="side-panel__item side-panel__logo"><p>LOGO</p></li>
+        <li className="side-panel__item"><NavLink to="/dashboard"><img src="icons/dashboard.svg" alt="dashboard"/></NavLink></li>
+        <li className="side-panel__item"><NavLink to="/progress"><img src="icons/progress.svg" alt="progress"/></NavLink></li>
+        <li className="side-panel__item"><NavLink to="/profile"><img src="icons/profile.svg" alt="profile"/></NavLink></li>
+        <li className="side-panel__item"><NavLink to="/settings"><img src="icons/settings.svg" alt="settings"/></NavLink></li>
       </ul>
     );
   }
