639507bd81d8928a73944d3ca46f8fda47c15023
alexluong <aluong@crimson.ua.edu>
2018-04-18 21:14:35 -0500

    Side panel to change active color

--- client/src/components/SidePanel.js
+++ client/src/components/SidePanel.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
-import { NavLink } from 'react-router-dom';
+import { withRouter }       from 'react-router';
+import { NavLink }          from 'react-router-dom';
 
-export default class SidePanel extends Component {
+class SidePanel extends Component {
+  renderLink(page) {
+    const active = this.props.location.pathname.slice(1);
+    let href = `/${page}`;
+    let src  = `icons/${page}${ active === page ? '-active' : ''}.svg`;
+    let alt  = page;
+    return <NavLink to={href}><img src={src} alt={alt} /></NavLink>;
+  }
+
   render() {
     return (
       <ul className="side-panel">
         <li className="side-panel__item side-panel__logo"><p>LOGO</p></li>
-        <li className="side-panel__item"><NavLink to="/dashboard"><img src="icons/dashboard.svg" alt="dashboard"/></NavLink></li>
-        <li className="side-panel__item"><NavLink to="/progress"><img src="icons/progress.svg" alt="progress"/></NavLink></li>
-        <li className="side-panel__item"><NavLink to="/profile"><img src="icons/profile.svg" alt="profile"/></NavLink></li>
-        <li className="side-panel__item"><NavLink to="/settings"><img src="icons/settings.svg" alt="settings"/></NavLink></li>
+        <li className="side-panel__item">{ this.renderLink('dashboard') }</li>
+        <li className="side-panel__item">{ this.renderLink('progress' ) }</li>
+        <li className="side-panel__item">{ this.renderLink('profile'  ) }</li>
+        <li className="side-panel__item">{ this.renderLink('settings' ) }</li>
       </ul>
     );
   }
 }
+
+export default withRouter(SidePanel);
